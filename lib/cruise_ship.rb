badf61a898b9c241d1e74f5d246fc242316366fb
Richard Hanolt <rhanolt@gmail.com>
2020-05-08 22:04:44 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -5,11 +5,3 @@
 # suite_d: "Charlie Chaplin",
 # suite_e: "Crumpet the Elf"
 # }
-
-def select_winner(passengers)
-  passengers.each do |key, value|
-    if key == :suite_a && value.start_with?("A")
-      return passengers[:suite_a]
-    end
-  end
-end
